a1672a6e42c90a8e91a9b3f190c015f1d9cd63ae
aureliowozhiak <aureliowozhiak@gmail.com>
2020-07-13 21:01:16 -0300

    Create and Config

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Generic_CRUD_ASP.NET_CORE_3._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Generic_CRUD_ASP.NET_CORE_3._1.Models
+{
+    public class Product
+    {
+    }
+}
